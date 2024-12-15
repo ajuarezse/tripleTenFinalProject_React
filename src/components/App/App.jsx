d1db3f433151038f5b0fd7e78b341574e482b869
ajuarezse <ajuarez.se@gmail.com>
2024-12-14 16:54:11 -0800

    install and impliment browser router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+//import { useState } from "react";
 //import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+//import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "../Header/Header";
 
@@ -9,6 +10,9 @@ function App() {
     <>
       <div className="app">
         <Header />
+        <Routes>
+          <Route></Route>
+        </Routes>
       </div>
       <h1>Hello lily</h1>
       <p>This is my final triple ten project!</p>
