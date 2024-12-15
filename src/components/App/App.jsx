cadbf30ecc1f703c3e289aba4ad8c98f4c8c68ec
ajuarezse <ajuarez.se@gmail.com>
2024-12-14 16:27:44 -0800

    buildout file structure and start header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+//import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "../Header/Header";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="app">
+        <Header />
+      </div>
+      <h1>Hello lily</h1>
       <p>This is my final triple ten project!</p>
     </>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+//import logo from "../../assets/logo.svg";
+//import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+//import { Link } from "react-router-dom";
+//import { useContext } from "react";
+//import CurrentUserContext from "../../contexts/CurrentUserContext/CurrentUserContext";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo">Name of Project</div>
+      <div className="header__auth-buttons">
+        <button className="header__auth-link">Sign Up</button>
+        <button className="header__auth-link">Log In</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
