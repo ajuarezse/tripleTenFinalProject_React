0f1103bc7d8403e350268be635d7be0171644561
ajuarezse <ajuarez.se@gmail.com>
2024-12-14 17:00:07 -0800

    create footer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,18 +4,20 @@ import { Routes, Route } from "react-router-dom";
 //import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
     <>
       <div className="app">
         <Header />
+        <h1>Hello lily</h1>
+        <p>This is my final triple ten project!</p>
         <Routes>
           <Route></Route>
         </Routes>
+        <Footer />
       </div>
-      <h1>Hello lily</h1>
-      <p>This is my final triple ten project!</p>
     </>
   );
 }
