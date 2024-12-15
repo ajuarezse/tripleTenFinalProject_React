fb6890e83f8729c3dc51530200068f5752403680
ajuarezse <ajuarez.se@gmail.com>
2024-12-14 19:30:59 -0800

    link main to header logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
 import "./Header.css";
 //import logo from "../../assets/logo.svg";
 //import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 //import { useContext } from "react";
 //import CurrentUserContext from "../../contexts/CurrentUserContext/CurrentUserContext";
 
 function Header() {
   return (
     <header className="header">
-      <div className="header__logo">Name of Project</div>
+      <Link to="/">
+        <div className="header__logo">Name of Project</div>
+      </Link>
       <div className="header__auth-buttons">
         <button className="header__auth-link">Sign Up</button>
         <button className="header__auth-link">Log In</button>
