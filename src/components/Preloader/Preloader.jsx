39e938c96316898847289f3b54b658dfcf29cd50
ajuarezse <ajuarez.se@gmail.com>
2024-12-17 22:23:36 -0800

    add preloader

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,11 @@
+import "./Preloader.css";
+
+function Preloader() {
+  return (
+    <div className="circle-preloader">
+      <p className="text-preloader">Searching for lyrics...</p>
+    </div>
+  );
+}
+
+export default Preloader;
