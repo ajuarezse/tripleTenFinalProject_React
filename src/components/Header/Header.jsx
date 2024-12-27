1c28d881f4fa688bff9c336e5af888313a4bc969
ajuarezse <ajuarez.se@gmail.com>
2024-12-26 20:04:10 -0800

    style generaly

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
+//import jarochoLogo from "../../assets/jarochoLogo.png";
 
 function Header({ handleRegisterModal, handleLoginModal, handleLogOut }) {
   return (
     <header className="header">
-      <Link to="/">
-        <div className="header__logo">Son Jarocho Songbook</div>
+      <Link to="/" className="header__logo-link">
+        {/*<img className="header__logo" src={jarochoLogo} alt="logo" />*/}
+        <div className="header__title">Son Jarocho Songbook</div>
       </Link>
       <section className="header__navigation">
         <Navigation
