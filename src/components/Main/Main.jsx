e29d82f414d15468b8a745c4d62cdaac54c2d521
ajuarezse <ajuarez.se@gmail.com>
2024-12-27 16:00:52 -0800

    adjust gradients and margins

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,7 +6,10 @@ import ItemCard from "../ItemCard/ItemCard";
 function Main({ likedSongs, onLike, handleCardClick }) {
   return (
     <main className="main">
-      <h1>All Songs</h1>
+      <h1 className="main__title">Main page headline goes here with words</h1>
+      <h2 className="main__subtitle">
+        Subtitle with instructions will go here with user guidance
+      </h2>
       <div className="main__songs-list">
         {mockLyrics.map((song) => (
           <ItemCard
