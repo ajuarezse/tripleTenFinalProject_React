990a32a879477ed834e7619509f02d01760629cf
ajuarezse <ajuarez.se@gmail.com>
2024-12-30 23:12:00 -0800

    set up sections

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,37 @@
 import "./About.css";
+import musicPhoto from "../../assets/jarochoInstruments.jpg";
+import myPhoto from "../../assets/aJuarez.jpg";
 
 function About() {
   return (
     <div className="about">
-      <section className="about__project">
-        <h2 className="about__project-title">About the Project</h2>
+      <section className="about__section">
+        <img
+          src={musicPhoto}
+          alt="arrangment of Son Jarocho Instruments"
+          className="about__image"
+        />
+        <div className="about__description">
+          <h2 className="about__title">About the Project</h2>
+          <p className="about__paragraph">
+            This block describes the project author. Here you should indicate
+            your name, what you do, and which development technologies you know.
+            You can also talk about your experience with TripleTen, what you
+            learned there, and how you can help potential customers.
+          </p>
+        </div>
       </section>
-      <section className="about__me">
-        <h2 className="about__me-title">About Me</h2>
+      <section className="about__section">
+        <img src={myPhoto} alt="Photo of Author" className="about__image" />
+        <div className="about__description">
+          <h2 className="about__title">About Me</h2>
+          <p className="about__paragraph">
+            This block describes the project author. Here you should indicate
+            your name, what you do, and which development technologies you know.
+            You can also talk about your experience with TripleTen, what you
+            learned there, and how you can help potential customers.
+          </p>
+        </div>
       </section>
     </div>
   );
