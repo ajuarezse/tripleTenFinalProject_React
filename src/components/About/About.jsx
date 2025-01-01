e96991af741788674bae3c77818a13b50a1d1f73
ajuarezse <ajuarez.se@gmail.com>
2024-12-31 19:06:34 -0800

    add about project and author

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import "./About.css";
 import musicPhoto from "../../assets/jarochoInstruments.jpg";
-import myPhoto from "../../assets/aJuarez.jpg";
+import myPhoto from "../../assets/profilePhoto.jpg";
 
 function About() {
   return (
@@ -12,26 +12,30 @@ function About() {
           className="about__image"
         />
         <div className="about__description">
-          <h2 className="about__title">About the Project</h2>
+          <h2 className="about__title">About the project</h2>
           <p className="about__paragraph">
-            This block describes the project author. Here you should indicate
-            your name, what you do, and which development technologies you know.
-            You can also talk about your experience with TripleTen, what you
-            learned there, and how you can help potential customers.
+            Imagine a party in Mexico where people play amazing music and dance!
+            That's Son Jarocho! It's old-school music from the Veracruz region.
+            But sadly, it was starting to disappear. So, this project is here to
+            help save it! You can explore the words to these awesome songs by
+            clicking on the cards on the main page. And the best part? You can
+            even listen to the music right here thanks to Spotify! Enjoy!
           </p>
         </div>
       </section>
       <section className="about__section">
-        <img src={myPhoto} alt="Photo of Author" className="about__image" />
         <div className="about__description">
-          <h2 className="about__title">About Me</h2>
+          <h2 className="about__title">About the author</h2>
           <p className="about__paragraph">
-            This block describes the project author. Here you should indicate
-            your name, what you do, and which development technologies you know.
-            You can also talk about your experience with TripleTen, what you
-            learned there, and how you can help potential customers.
+            Hi! I'm Angel, a student software developer who loves building cool
+            projects. I grew up playing Son Jarocho music in Southern
+            California, and I'm passionate about sharing my love for this
+            vibrant tradition. When I'm not coding or playing music, you can
+            find me challenging my friends to a chess match. I'm excited to
+            share my love for Son Jarocho with you through this project.
           </p>
         </div>
+        <img src={myPhoto} alt="Photo of Author" className="about__image" />
       </section>
     </div>
   );
